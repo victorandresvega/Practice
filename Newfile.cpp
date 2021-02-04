5c1dc69e86fbd48af9468e746428ef193a680df4
victorandresvega <victor.vega6@upr.edu>
2021-02-04 06:51:11 -0800

    Merge

--- Newfile.cpp
+++ Newfile.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main(){
     cout<<"This a new file in the develop branch"<<endl;
-    cout<<"Change to develop branch";
+    cout<<"Change to develop branch"<<endl<<"Merge with master branch";
     return 0;
 }
